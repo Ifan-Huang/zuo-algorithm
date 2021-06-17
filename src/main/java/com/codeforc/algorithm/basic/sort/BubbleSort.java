3ee549a8d710f53eb52417aeb1bf696b8cc6e7fb
IfanHuang <ifanhuang@aliyun.com>
2021-06-17 16:09:50 +0800

    添加doc文档

--- src/main/java/com/codeforc/algorithm/basic/sort/BubbleSort.java
+++ src/main/java/com/codeforc/algorithm/basic/sort/BubbleSort.java
@@ -1,6 +1,21 @@
-package com.codeforc.algorithm.basic.sort;/**
+package com.codeforc.algorithm.basic.sort;
+
+/**
  * Author: Ifan·Huang
- * Description: TODO
+ * Description: 冒泡排序，遍历样本数据，两两对比，如果下标小的元素比小标大的元素大，则交换位置。
  **/
-public class BubbleSort {
+public class BubbleSort implements Sortable {
+
+    /**
+     * 遍历样本数组，两两对比，将大的数组
+     * @param nums
+     */
+    @Override
+    public void sort(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j <nums.length; j++) {
+
+            }
+        }
+    }
 }
